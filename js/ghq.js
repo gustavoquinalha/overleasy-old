bd62d2200613c9e527e4e7dac421f6969bcdb5c0
gustavoquinalha <BeeuzOne@Beeuzs-Mac-mini.local>
2016-06-28 13:27:43 -0300

    index

--- js/ghq.js
+++ js/ghq.js
@@ -6,8 +6,9 @@ function CharactersCtrl($scope) {
     {
       "id": 1,
       "name": 'Facebook',
+      "color": '#3B5998',
       "icon": 'fa fa-facebook',
-      "image":'../img',
+      "image":'../img/facebook.svg',
       "link": 'facebook.html',
       "medida": [
          {
@@ -21,7 +22,9 @@ function CharactersCtrl($scope) {
     {
       "id": 2,
       "name": 'Youtube',
+      "color": '#E62117',
       "icon": 'fa fa-youtube',
+       "image":'../img/facebook.svg',
       "link": 'youtube.html',
       "medida": [
          {
@@ -33,10 +36,28 @@ function CharactersCtrl($scope) {
         ]
     },
     {
-      "id": 2,
+      "id": 3,
       "name": 'Twitter',
+      "color": '#00ABF1',
       "icon": 'fa fa-twitter',
+       "image":'../img/facebook.svg',
       "link": 'twitter.html',
+      "medida": [
+         {
+          "capa": '851x315'
+         },
+         {
+          "foto": '851x315'
+         }
+        ]
+    },
+    {
+      "id": 4,
+      "name": 'Linkedin',
+      "color": '#0177B5',
+      "icon": 'fa fa-linkedin',
+       "image":'../img/facebook.svg',
+      "link": 'linkedin.html',
       "medida": [
          {
           "capa": '851x315'
